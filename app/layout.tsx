b5f0cdbc9b38509ac2c51fc60bfdfec2213aabd5
Achen2089 <anthonychen2089@gmail.com>
2024-07-15 17:18:09 -0700

    change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Zenfit",
-  description: "AI for personalized training and support",
+  title: "SCT",
+  description: "Scientific Thinking Model for CS6795",
 };
 
 export default function RootLayout({
