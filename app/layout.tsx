6db48f518185eaa1986d9eba40baef6771d17fee
Achen2089 <anthonychen2089@gmail.com>
2024-07-18 17:59:21 -0700

    change zenfit to zentrain

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Zenfit",
+  title: "Zentrain",
   description: "AI for personalized training and support",
 };
 
